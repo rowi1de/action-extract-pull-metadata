b7e846c3f40f61fa6b513f490e36652cafe087b7
rowi1de <robert@scalable.capital>
2019-12-12 15:29:22 +0100

    Just log the json

--- src/main.ts
+++ src/main.ts
@@ -16,9 +16,7 @@ export async function run() {
 
     //See https://octokit.github.io/rest.js/
     const client = new github.GitHub(repoToken)
-
-    const includeDraft : Boolean =  Boolean(core.getInput('include-draft') || false) 
-    const pull = await client.pulls.get(
+     const pull = await client.pulls.get(
       {
         owner: issue.owner,
         repo: issue.repo,
@@ -26,39 +24,18 @@ export async function run() {
       }
     )
 
-    //Skip DRAFT PRs
-    if(pull.data.draft == false && !includeDraft){
-      console.log('Skipped: DRAFT Pull Request, not assigning PR.')
-      return
-    }
+    const files = await client.pulls.listFiles({
+      owner: issue.owner,
+      repo: issue.repo,
+      pull_number: issue.number
+    })
 
-    const skipWithNumberOfReviewers : number =  Number(core.getInput('skip-with-manual-reviewers') || Number.MAX_VALUE) 
-    const numberOfReviwers = pull.data.requested_reviewers?.length || 0
-    if(numberOfReviwers >= skipWithNumberOfReviewers){
-      console.log('Skipped: Already ' + numberOfReviwers + ' assigned reviwers, not assigning PR.')
-      return
-    }
-
-    const teams = core.getInput('teams').split(',').map(a => a.trim())
-    const persons = core.getInput('persons').split(',').map(a => a.trim())
+    //needs to go to lambda
+    console.info(JSON.stringify(files));
+  
+    //needs to go to lambda
+    console.info(JSON.stringify(pull));
     
-    if(teams.length == 0 && persons.length == 0){
-      core.setFailed("Please specify 'teams' and/or 'persons'")
-      return
-    }
-    else{
-      console.log("Adding teams: " + teams + ", persons: " + persons)
-    }
-
-    await client.pulls.createReviewRequest(
-      {
-        owner: issue.owner,
-        repo: issue.repo,
-        pull_number: issue.number,
-        reviewers: persons,
-        team_reviewers: teams
-      }
-    )
   } catch (error) {
     core.setFailed(error.message)
     throw error
