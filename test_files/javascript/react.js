ead2900f08f1e5f54088718ed88c33714b1a9f80
Kruno Grcevic <krunoslav.grcevic@scalable.capital>
2019-12-13 15:17:16 +0100

    test

--- test_files/javascript/react.js
+++ test_files/javascript/react.js
@@ -8,7 +8,7 @@ class HelloWorld extends React.Component {
     render() {
           return (
                   <div>
-                    Hello, React!
+                    Hello, React!!
                   </div>
                 )
         }
